122c8fcef90f474763637dc168aed4453468e9ec
Rodrigo Santos <rodrigo.santos@DEV213.m2mrj.local>
2019-09-30 17:08:44 -0300

    seguindo com o curso

---
+++ lendo_dados_internet3.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split  
+
+uri = "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv"
+dados = pd.read_csv(uri)
+
+print(dados.head())
+
+mapa = {
+    "home" : "principal", 
+    "how_it_works" : "como_funciona", 
+    "contact" : "contato", 
+    "bought" : "comprou"
+}
+
+dados = dados.rename(columns = mapa)
+
+x = dados[["principal","como_funciona","contato"]]
+y = dados["comprou"]
+
+SEED = 20 
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x,y,test_size=0.25,random_state=SEED,stratify=y) 
+
+print(treino_x.shape) 
+print(teste_x.shape)
+
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC() 
+modelo.fit(treino_x,treino_y) 
+
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y,previsoes) * 100 
+print("A acuracia foi %.2f%%" % acuracia) 
+
+print(treino_y.value_counts())
+print(teste_y.value_counts())
